a5fe522db8b356a4c4239d30b3a4af87ca983ee5
rofikul-resan <resan6231@gmail.com>
2023-08-16 22:36:26 +0600

    add footer section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,10 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="light">
       <body className={inter.className}>
-        <main>
-          <Navbar />
-          {children}
-        </main>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
