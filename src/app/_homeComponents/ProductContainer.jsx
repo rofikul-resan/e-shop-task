f963e02506ed4ef1d13fd2c14fb22f82f7653c1e
rofikul-resan <resan6231@gmail.com>
2023-08-17 00:47:22 +0600

    add spinner for data load

--- src/app/_homeComponents/ProductContainer.jsx
+++ src/app/_homeComponents/ProductContainer.jsx
@@ -3,6 +3,7 @@ import { getProduct } from "@/RTK-state/Sclice/ProductSlice";
 import ItemCard from "@/components/ItemCard";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { ClipLoader } from "react-spinners";
 
 const ProductContainer = () => {
   const products = useSelector((state) => state.products);
@@ -13,7 +14,11 @@ const ProductContainer = () => {
   console.log(products);
   return (
     <div className="py-10">
-      resan
+      {products.length === 0 && (
+        <div className="h-64 w-full flex items-center justify-center">
+          <ClipLoader color={"black"} size={50} />
+        </div>
+      )}
       <div className="grid md:grid-cols-3 md:w-9/12 mx-auto gap-6">
         {products.map((product, index) => (
           <ItemCard key={index} product={product} />
